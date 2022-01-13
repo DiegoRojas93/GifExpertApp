86bbd5155049743642ba1490e34c6330a266fb0f
Diego Rojas <diegorojas431@gmail.com>
2022-01-12 21:20:19 -0500

    Creando una lista

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function GifExpertApp() {
+
+  const [categories, setCategories] = useState(
+    ['One Punch', 'Samurai X', 'Dragon Ball']
+  );
+
+  const handleApp = title => {
+    setCategories( cats => [ ...cats, title ] )
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={() => handleApp('SevenDailySins')}> Add </button>
+
+      <ol>
+        {
+          categories.map( category => <li key={ category }>{ category }</li>)
+        }
+      </ol>
     </>
   )
 }
